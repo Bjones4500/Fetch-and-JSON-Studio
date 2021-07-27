8abf75f60dc2b2fe6bd729b1543e197f4b39f07d
Bjones4500 <BruceJones1983@gmail.com>
2021-07-26 20:12:35 -0500

    1st commit

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response) {
+      response.json().then(function(json) {
+        const div = document.getElementById("container");
+        let inner = "";
+        json.sort((a1, a2) => a1.hoursInSpace < a2.hoursInSpace);
+        for (let i = 0; i < json.length; i++) {
+          inner += addHtml(json[i]);
+        }
+        div.innerHTML= inner;
+        document.getElementById("count").innerHTML = json.length;
+      });
+    });
+  });
+  
+  function addHtml(data) {
+    
+    let astronaut = `
+      <div class="astronaut">
+      <div class="bio">
+        <h3>${data.firstName} ${data.lastName}</h3>
+        <ul>
+           <li>Hours in space: ${data.hoursInSpace}</li>
+           <li id="${data.active ? "green" : ""}">Active: ${data.active}</li>
+           <li>Skills: ${data.skills.join(", ")}</li>
+        </ul>
+     </div>
+     <img class="avatar" src="${data.picture}">
+    </div>
+    `;
+    return astronaut;
+  }
+  
